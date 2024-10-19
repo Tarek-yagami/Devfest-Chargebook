14ffd6083b3e5604206309712ce83910e60b9979
SaraBerrazouane <152658467+SaraBerrazouane@users.noreply.github.com>
2024-10-19 11:50:11 -1200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Single import of Route
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import MainPage from './components/MainPage'; 
 import FinancialReportGenerator from './components/FinancialReportGenerator'; 
 import ExpenseInsights from './components/ExpenseInsights';
@@ -11,14 +11,14 @@ import SignupPage3 from './pages/signuppage3';
 
 const App = () => {
   return (
-    <Router> {/* Ensure you're using Router here */}
+    <Router> 
       <Routes>
         <Route path="/signup" element={<SignupPage />} /> 
         <Route path="/signup/signup2" element={<SignupPage2 />} /> 
         <Route path="/signup/signup2/signup3" element={<SignupPage3 />} /> 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<LandingPage />} />
-        <Route path="/main" element={<MainPage />} /> {/* Adjusted to avoid conflict */}
+        <Route path="/main" element={<MainPage />} /> 
         <Route path="/reports" element={<FinancialReportGenerator />} />
         <Route path="/expenses" element={<ExpenseInsights />} />
       </Routes>
