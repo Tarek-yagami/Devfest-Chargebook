ef636bfa806a0440d0160d5c0fdd8c588fa3450f
SaraBerrazouane <152658467+SaraBerrazouane@users.noreply.github.com>
2024-10-19 11:49:08 -1200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Single import of Route
+import MainPage from './components/MainPage'; 
+import FinancialReportGenerator from './components/FinancialReportGenerator'; 
+import ExpenseInsights from './components/ExpenseInsights';
 import LoginPage from './pages/loginPage';
 import SignupPage from './pages/signupPage';
 import LandingPage from './pages/landingPage';
 import SignupPage2 from './pages/signuppage2';
 import SignupPage3 from './pages/signuppage3';
 
-// App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainPage from './components/MainPage'; 
-import FinancialReportGenerator from './components/FinancialReportGenerator';
-import ExpenseInsights from './components/ExpenseInsights'; 
-
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router> {/* Ensure you're using Router here */}
       <Routes>
         <Route path="/signup" element={<SignupPage />} /> 
         <Route path="/signup/signup2" element={<SignupPage2 />} /> 
         <Route path="/signup/signup2/signup3" element={<SignupPage3 />} /> 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<LandingPage />} />
-        <Route path="/" element={<MainPage />} />
+        <Route path="/main" element={<MainPage />} /> {/* Adjusted to avoid conflict */}
         <Route path="/reports" element={<FinancialReportGenerator />} />
         <Route path="/expenses" element={<ExpenseInsights />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
